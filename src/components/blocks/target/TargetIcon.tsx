6763b754644087b11f725e7d6b3e581456365e31
Pasha Rezero <kivachuk.2018@gmail.com>
2024-08-28 17:26:11 +0300

    some refactor target modal,icon components

--- src/components/blocks/target/TargetIcon.tsx
+++ src/components/blocks/target/TargetIcon.tsx
@@ -4,12 +4,20 @@ import HomeIcon from "@mui/icons-material/Home";
 import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
 import PublicIcon from "@mui/icons-material/Public";
 
-const iconMapping: Record<string, JSX.Element> = {
+export const iconMapping: Record<string, JSX.Element> = {
   add: <AddIcon style={{ color: "white", fontSize: "35px" }} />,
   home: <HomeIcon style={{ color: "white", fontSize: "35px" }} />,
   phone: <PhoneAndroidIcon style={{ color: "white", fontSize: "35px" }} />,
   travel: <PublicIcon style={{ color: "white", fontSize: "35px" }} />,
 };
+
+export const availableColors = [
+  "bg-gray-400",
+  "bg-red-600",
+  "bg-green-500",
+  "bg-blue-500",
+  "bg-yellow-500",
+];
 
 export const TargetIcon = ({
   selectedIcon,
