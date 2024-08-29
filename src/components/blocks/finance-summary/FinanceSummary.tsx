7a1be4ffb0b64ee4cbcf50199e22099670481a34
Pasha Rezero <kivachuk.2018@gmail.com>
2024-08-29 13:16:31 +0300

    create finance summary component

---
+++ src/components/blocks/finance-summary/FinanceSummary.tsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import { apiKey } from "../../../utils/apiKeys";
+
+export const FinanceSummary = () => {
+  const [exchangeRate, setExchangeRate] = useState(null);
+  const currentMonth = new Date().toLocaleString("ru-RU", { month: "long" });
+  const financeSummaryData = [
+    {
+      title: "Доход",
+      amount: 12000,
+    },
+    {
+      title: "Расход",
+      amount: 8000,
+    },
+    {
+      title: "Баланс",
+      amount: 15000,
+    },
+    {
+      title: "Долги",
+      amount: 5000,
+    },
+  ];
+
+  useEffect(() => {
+    const fetchExchangeRate = async () => {
+      try {
+        const response = await fetch(
+          `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`
+        );
+        const data = await response.json();
+        setExchangeRate(data.conversion_rates.RUB.toFixed(2));
+      } catch (error) {
+        console.error("Ошибка при получении курса валют:", error);
+      }
+    };
+
+    fetchExchangeRate();
+  }, []);
+
+  const formatNumber = (num: number) => {
+    return new Intl.NumberFormat("ru-RU").format(num);
+  };
+
+  return (
+    <div
+      className="w-[75%] h-[150px] text-white py-4 px-5 border border-gray-300 rounded-lg overflow-y-auto shadow-lg shadow-gray-300 mb-5"
+      style={{
+        background: `
+          linear-gradient(
+            to top right, 
+            #9C27B0 0%, 
+            transparent 50%
+          ), 
+          linear-gradient(
+            to right, 
+            #3b82f6 8%, 
+            #6366f1 100%
+          )
+        `,
+      }}
+    >
+      <div className="flex justify-between font-bold mb-4">
+        <h1 className="text-xl">
+          {currentMonth.charAt(0).toUpperCase() + currentMonth.slice(1)}
+        </h1>
+        <p className="text-sm font-semibold bg-gray-200 bg-opacity-10 rounded-lg px-2 py-1">
+          {exchangeRate ? `1 USD = ${exchangeRate} рубля` : "Загрузка..."}
+        </p>
+      </div>
+      <div className="flex justify-center gap-4">
+        {financeSummaryData.map((item) => (
+          <div
+            className="w-[25%] bg-gray-200 bg-opacity-10 rounded-lg p-2"
+            key={item.title}
+          >
+            <p className="text-xl font-bold">{formatNumber(item.amount)} ₽</p>
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,28 +5,32 @@ import ExpensesAddModal from "../components/modals/ExpensesAddModal";
 import { SidebarComponent } from "../components/sidebar/SidebarComponent";
 import { TargetBlockComponent } from "../components/blocks/target/TargetComponent";
 import { DebtsBlock } from "../components/blocks/debt/DebtsBlock";
+import { FinanceSummary } from "../components/blocks/finance-summary/FinanceSummary";
 const MainPage = () => {
   const [modal, toggleModal] = useState(false);
 
   return (
-    <div className="grid" style={{ gridTemplateColumns: "13% 87%" }}>
+    <div className="grid gap-5" style={{ gridTemplateColumns: "13% 84%" }}>
       <SidebarComponent />
-      <div className="div grid" style={{ gridTemplateColumns: "75% 25%" }}>
-        <div className="grid">
-          <AccountBlock text={"Счета"}></AccountBlock>
-          <SpendingBlock SpendingTitle={"Расходы"}></SpendingBlock>
+      <div className="mt-5">
+        <FinanceSummary />
+        <div className="div grid" style={{ gridTemplateColumns: "75% 25%" }}>
+          <div className="grid">
+            <AccountBlock text={"Счета"}></AccountBlock>
+            <SpendingBlock SpendingTitle={"Расходы"}></SpendingBlock>
+          </div>
+          <div>
+            <TargetBlockComponent />
+            <DebtsBlock />
+          </div>
+          <button
+            className=" p-2 pr-5 pl-5 border"
+            onClick={() => toggleModal(true)}
+          >
+            Add Expenses
+          </button>
+          {modal && <ExpensesAddModal toggleModal={toggleModal} />}
         </div>
-        <div>
-          <TargetBlockComponent />
-          <DebtsBlock />
-        </div>
-        <button
-          className=" p-2 pr-5 pl-5 border"
-          onClick={() => toggleModal(true)}
-        >
-          Add Expenses
-        </button>
-        {modal && <ExpensesAddModal toggleModal={toggleModal} />}
       </div>
     </div>
   );
